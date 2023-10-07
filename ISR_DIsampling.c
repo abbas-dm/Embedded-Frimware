00bbf8a0f7a1f21fec5501d8d19dc8af4f32c93a
D Mabu Jaheer Abbas <56586584+abbas-dm@users.noreply.github.com>
2023-10-07 11:21:04 +0530

    Update ISR_DIsampling.c

--- ISR_DIsampling.c
+++ ISR_DIsampling.c
@@ -1,91 +1,77 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <time.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-// typedef unsigned char uint8_t
-uint8_t g_ReadDIpinSts = 255;
+// unsigned char uint8_t
+uint8_t g_ReadDIpinSts = 0;
 uint8_t g_AppDIpinSts = 0;
 
-// Global Variables to be used in the logic of ISR function
-int count[8] = {0,0,0,0,0,0,0,0};
-int Readpins[8] = {0,0,0,0,0,0,0,0};
+/* 
+ * counter to check whether values are changed or not
+ * resets to zero every time new input has come or counter > 10 
+*/
+uint8_t counter = 0;
 
-// Implementing the function to find bit in a given position
-int bitFind(int n) {
-
-    int bit;
-    bit = (( g_ReadDIpinSts & (1 << (n) ) ) ? 1 : 0 );
-
-    return bit;
-}
+/*
+ * assuming the below array is the digital pins data
+ * received from CAN bus
+ * 
+ * In real this array will be updated from real CAN Bus
+*/
+uint8_t readDigitalPins[8] = {0,1,0,1,0,1,1,1};
 
 // Implementing Interrupt Service Routine function
-int ISR_DIsampling() {
+void ISR_DIsampling() {
 
-    // Write your code here
-    for (int i=0; i<8; i++) {
-        //Checking count values for the first time
-        //if count[i] == 0, update it to 1
-        //else checking for condition to increase count[i] or to store 1 in count[i]
-        if (count[i] == 0) {
-            count[i] = 1;
+    for (int i = 0; i < 8; i++) {
+        if (readDigitalPins[i] == 1) {
+            g_ReadDIpinSts |= (1 << i); // Set the bit to 1
+        } else {
+            g_ReadDIpinSts &= ~(1 << i); // Clear the bit to 0
         }
-        else {
-            if (bitFind(i) == Readpins[i]) {
-                count[i]++;
+    } 
+
+    for (int pin = 0; pin < 8; pin++) {
+        // Read the status of the pin from g_ReadDIpinSts
+        uint16_t currentStatus = (g_ReadDIpinSts >> pin) & 0x1;
+
+        // Check if the current status matches the previous status
+        if (currentStatus == ((g_AppDIpinSts >> pin) & 0x1)) {
+            // Increment the consecutive consistent readings counter for this pin
+            g_ConsistentCount[pin]++;
+
+            // Check if the threshold for consecutive consistent readings is reached
+            if (g_ConsistentCount[pin] >= 10) {
+                // Update the corresponding bit in g_AppDIpinSts
+                g_AppDIpinSts |= (currentStatus << pin);
+
+                // Reset the consecutive consistent readings counter for this pin
+                g_ConsistentCount[pin] = 0;
             }
-            else {
-                count[i] = 1;
-            }
+        } else {
+            // Reset the consecutive consistent readings counter for this pin
+            g_ConsistentCount[pin] = 0;
         }
-    }
-
-    // Storing the current Digital pin status into Readpins 
-    for (int i=0; i<8; i++) {
-        Readpins[i] = bitFind(i);
-    }
-
-    // Updating the g_AppDIpinSts variable
-    for (int i=0; i<8; i++) {
-        if(count[i] >= 10) {
-            if(bitFind(i) == 1) {
-                g_AppDIpinSts |= 1 << (i);
-            }
-            else {
-                g_AppDIpinSts &= ~(1 << (i));
-            }
-        }
-    }
-
-    return 0;
+    }  
 }
 
-// Implementing Delay function for 100 milli seconds
-void delay(int number_of_ms) {
-
-    // Set start_time to current time
-    clock_t start_time = clock();
-  
-    // Loop untill the desired delay is reached
-    while (clock() < start_time + number_of_ms)   
-    ;
+// Timer1 compare match ISR
+ISR(TIMER1_COMPA_vect) {
+    // Call the function to be executed every 100 ms
+    ISR_DIsampling();
 }
 
 int main() {
 
-    printf("\nPrinting g_AppDIpinSts values after every ISR Call :- \n");
+    // Initialize hardware and configure timer (Timer1 in this case)
+    // For example, configure Timer1 for CTC mode and prescaler 1024
+    TCCR1B |= (1 << WGM12);    // Set CTC mode
+    OCR1A = 1562;             // Set compare value for 100 ms at 16 MHz (Prescaler 1024)
+    TIMSK1 |= (1 << OCIE1A);   // Enable compare match interrupt
+    sei();
 
-    // While loop to run continuosly
+    // While loop to run continuously
     while (1) {
-        // ISR function calling
-        ISR_DIsampling();   
-
-        // 100 ms delay to trigger the ISR
-        delay(100);
-
-        // Printing the g_AppDIpinSts data after every ISR call
-        printf("g_AppDIpinSts value after every ISR call is :: %d", g_AppDIpinSts);
-        printf("\n\n");
+        
     }
 
     return 0;
